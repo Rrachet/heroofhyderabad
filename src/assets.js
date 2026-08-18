a5e7330f777c7fee913e108695e259a58f6d775b
Rrachet <136880232+Rrachet@users.noreply.github.com>
2026-08-18 11:21:05 +0530

    assets: add supplied HOHYD and Triumphs of Talent logos

---
+++ src/assets.js
@@ -0,0 +1,2 @@
+export const HOHYD_LOGO = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA4oAAAF9CAYAAAB7m1ZQAAA...';
+export const TOT_LOGO = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD...';
